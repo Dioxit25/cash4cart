6b4d90e7544d8564573a3bc8a0cabcb3b66cbc9c
Dioxit25 <Dioxit2510@gmail.com>
2025-11-11 12:20:50 +0300

    Add files via upload

---
+++ tonconnect-manifest.js
@@ -0,0 +1,7 @@
+{
+  "url": "https://Dioxit25.github.io/cash4cart",
+  "name": "Cash4Cart",
+  "iconUrl": "https://Dioxit25.github.io/cash4cart/icon.png",
+  "termsOfUseUrl": "https://Dioxit25.github.io/cash4cart/terms.html",
+  "privacyPolicyUrl": "https://Dioxit25.github.io/cash4cart/privacy.html"
+}
