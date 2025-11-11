c01abe2519c5f17cf8782a1cd593cd0212bed320
Dioxit25 <Dioxit2510@gmail.com>
2025-11-11 12:21:27 +0300

    Update tonconnect-manifest.js

--- tonconnect-manifest.js
+++ tonconnect-manifest.js
@@ -1,7 +1,7 @@
-{
+window.TON_CONNECT_MANIFEST = {
   "url": "https://Dioxit25.github.io/cash4cart",
   "name": "Cash4Cart",
   "iconUrl": "https://Dioxit25.github.io/cash4cart/icon.png",
   "termsOfUseUrl": "https://Dioxit25.github.io/cash4cart/terms.html",
   "privacyPolicyUrl": "https://Dioxit25.github.io/cash4cart/privacy.html"
-}
+};
